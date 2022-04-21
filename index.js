e112d0c9580ed6f671fd610f1cb587be7808a378
McKay <wmsmckay@gmail.com>
2022-04-21 14:54:21 -0600

    completed group project

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-    res.send('Gabe Itches')
-  })
+const professionalRoutes = require('./routes/professional');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+// app.get('/', (req, res) => {
+    // res.send('s')
+  // })
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Controll-Allow-Origin', '*');
+  next();
+});
+
+app.use('/professional', professionalRoutes);
 
 app.listen(port, () => {
     console.log(`App running on port: ${port}`)
-}) 
+});
