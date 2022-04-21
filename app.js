b414bb5cd5b6cf05bbc5055d49edd47e90b66df3
McKay <wmsmckay@gmail.com>
2022-04-21 16:22:29 -0600

    completed personal assignment

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+// const professionalRoutes = require('./routes/professional');
+// const contactRoutes = require('./routes/contacts');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`App running on port: ${port}`);
+  }
+});
+
+
+
+// app.listen(port, () => {
+//     console.log(`App running on port: ${port}`)
+// });
