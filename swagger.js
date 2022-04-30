98e1fcb4599d9824c942a66b466b68eb1c1f8dca
McKay <wmsmckay@gmail.com>
2022-04-30 10:01:48 -0600

    fixed path issue

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Description'
   },
-  host: 'https://cse341api.herokuapp.com/',
+  host: 'https://cse341api.herokuapp.com',
   schemes: ['http']
 };
 
